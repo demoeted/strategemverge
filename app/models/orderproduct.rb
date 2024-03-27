c6df70da3eea160bb321ed6d6a81a30e14c84fcc
Almar Prado <almarsamprado1@gmail.com>
2024-03-27 18:04:47 -0500

    Fixed: Error with db:migration causing foreign_keys to not work in Orderproducts. Added: About Us table and active admin resource.

--- app/models/orderproduct.rb
+++ app/models/orderproduct.rb
@@ -1,19 +1,19 @@
 class Orderproduct < ApplicationRecord
   belongs_to :order
-  has_many :case_fans
-  has_many :cases
-  has_many :computer_monitors
-  has_many :cpus
-  has_many :keyboards
-  has_many :motherboards
-  has_many :mice
-  has_many :optical_drives
-  has_many :powersupplies
-  has_many :rams
-  has_many :speakers
-  has_many :thermal_pastes
-  has_many :videocards
-  has_many :webcams
+  has_many :case_fans, foreign_key: 'product_id'
+  has_many :cases, foreign_key: 'product_id'
+  has_many :computer_monitors, foreign_key: 'product_id'
+  has_many :cpus, foreign_key: 'product_id'
+  has_many :keyboards, foreign_key: 'product_id'
+  has_many :motherboards, foreign_key: 'product_id'
+  has_many :mice, foreign_key: 'product_id'
+  has_many :optical_drives, foreign_key: 'product_id'
+  has_many :powersupplies, foreign_key: 'product_id'
+  has_many :rams, foreign_key: 'product_id'
+  has_many :speakers, foreign_key: 'product_id'
+  has_many :thermal_pastes, foreign_key: 'product_id'
+  has_many :videocards, foreign_key: 'product_id'
+  has_many :webcams, foreign_key: 'product_id'
 
   validates :quantity, presence: true
   validates :price, numericality: { only_numeric: true }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_03_27_185826) do
+ActiveRecord::Schema[7.1].define(version: 2024_03_27_191054) do
+  create_table "about_us", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
