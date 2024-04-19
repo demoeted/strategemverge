81bbf307e1c85d867b9bd8523dd3be8af2093333
Almar Prado <almarsamprado1@gmail.com>
2024-04-19 02:46:07 -0500

    Added: ability to items to cart. Other: displays some debug info if an item is in cart.

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,46 +1,60 @@
 class CartController < ApplicationController
-  # def create
+  def create
   #   logger.debug("Adding #{params[:quantity]} #{params[:category]} ID no. #{params[:id]}")
-  #   id = params[:id].to_i
-  #   category = params[:category]
-  #   quantity = params[:quantity].to_i
+    id = params[:id].to_i
+    category = params[:category]
+    quantity = params[:quantity].to_i
 
-  #   case category
-  #   when "Case Fans"
-  #     product = CaseFan.find(id)
-  #   when "Cases"
-  #     product = Case.find(id)
-  #   when "Monitors"
-  #     product = ComputerMonitor.find(id)
-  #   when "CPUs"
-  #     product = Cpu.find(id)
-  #   when "Keyboards"
-  #     product = Keyboard.find(id)
-  #   when "Mouses"
-  #     product = Mouse.find(id)
-  #   when "Optical Drives"
-  #     product = OpticalDrive.find(id)
-  #   when "Power Supplies"
-  #     product = Powersupply.find(id)
-  #   when "Memory"
-  #     product = Ram.find(id)
-  #   when "Speakers"
-  #     product = Speaker.find(id)
-  #   when "Thermal Pastes"
-  #     product = ThermalPaste.find(id)
-  #   when "Video Cards"
-  #     product = Videocard.find(id)
-  #   when "Webcams"
-  #     product = Webcam.find(id)
-  #   end
+    case category
+    when "Case Fans"
+      product = CaseFan.find(id)
+    when "Cases"
+      product = Case.find(id)
+    when "Monitors"
+      product = ComputerMonitor.find(id)
+    when "CPUs"
+      product = Cpu.find(id)
+    when "Keyboards"
+      product = Keyboard.find(id)
+    when "Mouses"
+      product = Mouse.find(id)
+    when "Optical Drives"
+      product = OpticalDrive.find(id)
+    when "Power Supplies"
+      product = Powersupply.find(id)
+    when "Memory"
+      product = Ram.find(id)
+    when "Speakers"
+      product = Speaker.find(id)
+    when "Thermal Pastes"
+      product = ThermalPaste.find(id)
+    when "Video Cards"
+      product = Videocard.find(id)
+    when "Webcams"
+      product = Webcam.find(id)
+    end
 
-  #   session[:cart] << Hash[:category, category, :id, id, :quantity, quantity] unless session[:cart][:id].include?(id)
-  #   redirect_to root_path
-  # end
+    is_clean = true
+    if session[:cart].present?
+      session[:cart].each do |p|
+        if p["category"] == category && p["id"] == id
+          is_clean = false
+        end
+      end
+    end
 
-  # def destroy
-  #   id = params[:id].to_i
+    if is_clean
+      session[:cart] << {:category=> category, :id=>id, :quantity=>quantity} 
+      flash[:notice] = " ➕ Added #{quantity} #{product.name} to cart."
+    end
 
-  #   redirect_to root_path
-  # end
+    redirect_to root_path
+  end
+
+  def destroy
+    id = params[:id].to_i
+    category = params[:category].to_s
+
+    redirect_to root_path
+  end
 end
